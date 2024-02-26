fbf0f0ba5bf088b59393881722766f12e197bc1c
WICKKKKK <493496526@qq.com>
2024-02-27 00:26:07 +0800

    update install linux bat

--- install_linux.sh
+++ install_linux.sh
@@ -1,6 +1,6 @@
-@echo off
+#!/bin/bash
 
-export HF_HOME=huggingface
+export HF_HOME=/workspace/local_ssd/huggingface
 export NO_PROXY=localhost,.woa.com,.oa.com,.tencent.com
 export HTTP_PROXY=$ENV_VENUS_PROXY
 export HTTPS_PROXY=$ENV_VENUS_PROXY
@@ -9,8 +9,8 @@ export http_proxy=$ENV_VENUS_PROXY
 export https_proxy=$ENV_VENUS_PROXY
 
 pip install torch==2.2.0 torchvision==0.17.0 --extra-index-url https://download.pytorch.org/whl/cu121
-pip install bitsandbytes==0.42.0
-pip install deepspeed==0.13.3
+pip install bitsandbytes==0.41.1
+pip install deepspeed==0.11.2
 pip install -r ./requirements.txt
 
 if [ $? -ne 0 ]; then
@@ -21,7 +21,7 @@ fi
 echo Install completed
 
 ln -s /dockerdata/ /workspace/local_ssd
-mkdir /workspace/local_ssd/models
+mkdir -p /workspace/local_ssd/models
 ln -s /group/30188/ /workspace/falcon_folder
 cp -r /workspace/falcon_folder/models/cogagent-vqa-hf /workspace/local_ssd/models/cogagent-vqa-hf
 
